73f93a56a59cb3d41a141b1b04430560fa11c958
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2022-05-18 14:11:19 -0400

    FIXING, STILL NOT RETURNING RESULTS

--- provider_customize.sql
+++ provider_customize.sql
@@ -1,22 +1,27 @@
+
+use role accountadmin;
+use warehouse app_wh;   
+
 //create table in publisher shared database
 create or replace table "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PUBLISHER_CUSTOMER" as select b.*,  a.genre, a.affinity_score
 from "CLEANROOM_AUDIENCE_SAMPLE"."SOURCE"."AFFINITY" a inner join "CLEANROOM_AUDIENCE_SAMPLE"."SOURCE"."SUBSCRIPTION" b
 on a.rec_id = b.rec_id;
 
 //create secure view 
-create or replace secure view "DCR_DEMOCUR_PROVIDER_DB"."CLEANROOM"."PROVIDER_PUBLISHER" as select * from "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PUBLISHER_CUSTOMER";
+create or replace secure view "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PROVIDER_PUBLISHER" as select * from "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PUBLISHER_CUSTOMER";
 
 //apply row access policy
-ALTER TABLE  "DCR_DEMOCUR_PROVIDER_DB"."CLEANROOM"."PROVIDER_PUBLISHER" add row access policy dcr_DEMOCUR_provider_db.shared_schema.data_firewall on (email);
+ALTER TABLE  "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PROVIDER_PUBLISHER" add row access policy dcr_DEMOCUR_provider_db.shared_schema.data_firewall on (email);
 
 //grant select on secure view to share
-grant select on "DCR_DEMOCUR_PROVIDER_DB"."CLEANROOM"."PROVIDER_PUBLISHER" to share dcr_DEMOCUR_data;
+grant select on "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PROVIDER_PUBLISHER" to share dcr_DEMOCUR_data;
+grant select on "DCR_DEMOCUR_PROVIDER_DB"."SHARED_SCHEMA"."PROVIDER_PUBLISHER" to share dcr_DEMOCUR_app; 
 
-
+select * from dcr_DEMOCUR_provider_db.templates.dcr_templates;
 
 //insert new template
 
-insert into dcr_DEMOCUR_provider_db.templates.dcr_templates (template_name, template, dp_sensitivity, dimensions) 
+insert into dcr_DEMOCUR_provider_db.templates.dcr_templates (template_name, template, dp_sensitivity, dimensions, template_type, procedure_header) 
 values ('audience_overlap',
 $$
 select
@@ -27,12 +32,10 @@ select
     , count(distinct p.user_id) as actual_overlap 
     , to_number(round({{ app_instance | sqlsafe }}.cleanroom.addNoise(count(distinct p.user_id),{{ epsilon | sqlsafe }},{{ app_instance | sqlsafe }}.cleanroom.ns_{{ request_id | sqlsafe }}()))) as dp_overlap 
 from
-    {{ app_data | sqlsafe }}.cleanroom.provider_publisher p,
-    {{ consumer_db | sqlsafe }}.{{ consumer_schema | sqlsafe }}.{{ brand_consumer_table | sqlsafe }} at(timestamp => '{{ at_timestamp | sqlsafe }}'::timestamp_tz) c
+    shared_schema.provider_publisher p,
+    {{ consumer_db | sqlsafe }}.{{ consumer_shared_data_schema | sqlsafe }}.{{ brand_consumer_table | sqlsafe }} at(timestamp => '{{ at_timestamp | sqlsafe }}'::timestamp_tz) c
 where
     c.{{ brand_consumer_join_field | sqlsafe }} = p.user_id
-    and exists (select table_name from {{ consumer_db | sqlsafe }}.information_schema.tables where table_schema = upper('{{ consumer_schema | sqlsafe }}') and table_name = upper('{{ consumer_table| sqlsafe }}') and table_type = 'BASE TABLE')
-    and ((select any_value(pid) from {{ app_data | sqlsafe }}.cleanroom.pid)=(select any_value(pid) from {{ app_instance | sqlsafe }}.cleanroom.pid))
    {% if  where_clause  %} 
     and ( {{ where_clause | sqlsafe }} )
     {% endif %}    
@@ -43,6 +46,8 @@ group by
     {% endfor %}
 having count(distinct p.user_id)  > 25
 order by count(distinct p.user_id) desc;
-$$,1,'c.country|c.marital|c.equipment_like|c.activity_like|c.age_range|c.state|c.lifestyle_like|c.gender|p.genre|p.bundle_user|p.subscription_type|p.product_name');
+$$,1,'c.country|c.marital|c.equipment_like|c.activity_like|c.age_range|c.state|c.lifestyle_like|c.gender|p.genre|p.bundle_user|p.subscription_type|p.product_name','SQL_immediate',NULL);
 
 select * from dcr_DEMOCUR_provider_db.templates.dcr_templates ;
+
+//delete from dcr_DEMOCUR_provider_db.templates.dcr_templates where template_name = 'audience_overlap';
